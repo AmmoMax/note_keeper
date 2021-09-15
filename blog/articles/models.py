fb723ce6e7e9311c673db8f2daf284b47d910de0
AmmoMax <steelratmax@gmail.com>
2021-09-15 22:59:01 +0300

    modified Author model and added __str__ method

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -7,13 +7,23 @@ class Note(models.Model):
     author = models.ManyToManyField(to='Author')
     tags = models.ManyToManyField(to='Tag')
     time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_created=True)
+    time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
